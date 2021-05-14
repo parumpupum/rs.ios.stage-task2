8503c39cd85ac0097728f5ceb848955fa88e481c
Kirill Hobyan <82779687+parumpupum@users.noreply.github.com>
2021-05-14 20:49:20 +0300

    Update Pairs.m

--- Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
@@ -6,7 +6,7 @@
     int count = 0;
     for (int i = 0; i < array.count; i++)
         for (int j = i + 1; j < array.count; j++){
-            if (ABS([array[i] intValue] - [array[j] intValue] == [number intValue]))count++;
+            if (ABS([array[i] intValue] - [array[j] intValue]) == [number intValue])count++;
         }
     
     return count;
